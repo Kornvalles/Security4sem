24eb60b56363b1de77bfd0ef515cdbb135c1d741
Mikkel Christoffersen <mikkelkornval@gmail.com>
2020-03-29 18:38:39 +0200

    Security demo

---
+++ demo_security/src/main/java/Demo.java
@@ -0,0 +1,44 @@
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author mikkel
+ */
+@Entity
+public class Demo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String info;
+
+    public Demo() {
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public Demo(String info) {
+        this.info = info;
+    }
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+}
